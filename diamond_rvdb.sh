0e254af07665c64c85d2ef138be76e64668ae436
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-01 10:38:12 +0200

    RVDB diamond run

---
+++ diamond_rvdb.sh
@@ -0,0 +1,40 @@
+#!/bin/env bash
+
+#load module
+module diamond
+
+# variables and directories
+wdir="/analyses/users/nokuzothan/disc_pipe/init_tools"
+current_dir="${wdir}/diamond"
+input_reads_dir="${wdir}/megahit/output/default"
+input_proteins="${current_dir}/input/protein_sequences_rvdb.fasta"
+output="${current_dir}/output"
+threads=$((`/bin/nproc` -2))
+
+#clear existing output directory if any, make new output directory 
+if [[ -e $output ]]; then
+  rm -rf ${output} 
+fi
+mkdir -p -m a=rwx ${output}
+
+#make diamond protein database
+diamond makedb --in ${input_proteins} -d ${output}/nr
+
+#loop through each of the files created in megahit output directory to find final.congtigs.fa files and run diamond
+for folder in ${input_reads_dir}/*; do
+
+  if [[ -d ${folder} ]]; then
+    sample=$(basename ${folder})
+    contigs=${folder}/final.contigs.fa
+
+
+    #alignment using blastx
+    if [[ -f ${contigs} ]]; then
+    sample_out=${output}/${sample}_rvdv.matches.m8
+    diamond blastx -d ${output}/nr.dmnd -q ${contigs} -o ${sample_out} --threads ${threads} -f 6
+
+    else 
+      echo "Contigs file for ${sample} not found."
+    fi
+  fi
+done
