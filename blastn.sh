32d0f3b2814a4362bc05a86495bc1b1fb09957fa
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-03 12:04:45 +0200

    Initial commit

---
+++ blastn.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash 
+
+#load modules
+module ncbi
+
+#directories
+
+#use previously made accession.txt file 
+
+#loop through each line of file to get accession number
+
+#search accession number to check if it matches any contig
+
