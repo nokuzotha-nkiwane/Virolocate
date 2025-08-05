f1dce3aaf6bdf682f02771271109be84b9dead41
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-05 15:03:53 +0200

    filename update

--- diamond_ncbi.sh
+++ diamond_ncbi.sh
@@ -1,63 +1,40 @@
 #!/bin/env bash
 
-#load modules
+#load module
 module diamond
 
-#directories
+# variables and directories
 wdir="/analyses/users/nokuzothan/disc_pipe"
-cdir="${wdir}/init_tools/diamond/output"
-db_coll="${wdir}/ncbidb/nr"
-db_fastas="${cdir}/db_fastas"
-db_output="${cdir}/databases"
+cdir="${wdir}/init_tools/diamond"
+input_reads_dir="${wdir}/init_tools/megahit/output/default"
+db="${wdir}/ncbidb/fasta/nr.gz"
+output="${cdir}/output/NCBI"
+threads=$((`/bin/nproc` -2))
 
-#make directory to recieve fastas
-if [[ -e ${db_fastas} ]]; then
-  rm -rf ${db_fastas} 
+#clear existing output directory if any, make new output directory 
+if [[ -e $output ]]; then
+  rm -rf ${output} 
 fi
-mkdir -p -m a=rwx ${db_fastas}
+mkdir -p -m a=rwx ${output}
 
-#make directory for diamond made databases
-if [[ -e ${db_output} ]]; then
-  rm -rf ${db_output} 
-fi
-mkdir -p -m a=rwx ${db_output}
+#make diamond protein database
+gunzip -c ${db} | diamond makedb --in - -d ${cdir}/nr
 
-#function to unzip each database batch and make a diamond database with resulting fastas
-function db_make() {
-    db_zipped=$1
-    db_fasta_loc=$2
-    output=$3
+#loop through each of the files created in megahit output directory to find final.congtigs.fa files and run diamond
+for folder in (ls ${input_reads_dir}/*); do
 
-    #unzip db_zipped from db_collection to a new location while keeping original zipped file 
-    tar -xzf ${db_zipped} -C ${db_fasta_loc}
+  if [[ -d ${folder} ]]; then
+    sample=$(basename ${folder})
+    contigs=${folder}/final.contigs.fa
 
-    #fasta file base names and locations
-    find "${db_fasta_loc}" -name '*.fasta' -o -name '*.fa' | while read -r fasta_file; do
-        name=$(basename "${fasta_file}")
-        out_db="${output}/${base_name%.fa*}"
 
-    #make database from fasta
-    diamond makedb --in ${fasta_file} -d ${out_}
-}
-export -f db_make
+    #alignment using blastx
+    if [[ -f ${contigs} ]]; then
+    sample_out=${output}/${sample}.matches.m8
+    diamond blastx -d ${cdir}/nr.dmnd -q ${contigs} -o ${sample_out} --threads ${threads} -f 6
 
-while read -r ${db_coll}/nr.*.tar.gz; do
-    ls ${db_coll}/nr.*.tar.gz | parallel -j 10 -n1 -I% db_make % ${db_fastas} ${db_fastas}/nr.*.fasta ${output}/nr 
+    else 
+      echo "Contigs file for ${sample} not found."
+    fi
+  fi
 done
-
-#db_fastas used can now be deleted
-if [[ -e ${db_fastas} ]]; then
-  rm -rf ${db_fastas} 
-fi
-
-
-
-
-
-#ls ${db_fasta}/nr.*.tar.gz | cut -d '.' -f1,2 | parallel -j 10 -n1 -I% "db_make % ${output}/nr"
-
-parallel -j 10 -n1 -I% db_make ${db_fasta}/nr.*.tar.gz ${output}/nr
-
-
-#ls ${db_fasta}/nr.*.tar.gz | cut -d '.' f1,2 | parallel -j 10 -n1 -I% "DBX % ${output}"
-#ls | cut -d '.' f1,2 | parallel -j 10 -n1 -I% "DBX % ${fasta} ${wdir} ${output}"
---
+++ diamond_ncbi_function_attempt.sh
@@ -0,0 +1,63 @@
+#!/bin/env bash
+
+#load modules
+module diamond
+
+#directories
+wdir="/analyses/users/nokuzothan/disc_pipe"
+cdir="${wdir}/init_tools/diamond/output"
+db_coll="${wdir}/ncbidb/nr"
+db_fastas="${cdir}/db_fastas"
+db_output="${cdir}/databases"
+
+#make directory to recieve fastas
+if [[ -e ${db_fastas} ]]; then
+  rm -rf ${db_fastas} 
+fi
+mkdir -p -m a=rwx ${db_fastas}
+
+#make directory for diamond made databases
+if [[ -e ${db_output} ]]; then
+  rm -rf ${db_output} 
+fi
+mkdir -p -m a=rwx ${db_output}
+
+#function to unzip each database batch and make a diamond database with resulting fastas
+function db_make() {
+    db_zipped=$1
+    db_fasta_loc=$2
+    output=$3
+
+    #unzip db_zipped from db_collection to a new location while keeping original zipped file 
+    tar -xzf ${db_zipped} -C ${db_fasta_loc}
+
+    #fasta file base names and locations
+    find "${db_fasta_loc}" -name '*.fasta' -o -name '*.fa' | while read -r fasta_file; do
+        name=$(basename "${fasta_file}")
+        out_db="${output}/${base_name%.fa*}"
+
+    #make database from fasta
+    diamond makedb --in ${fasta_file} -d ${out_}
+}
+export -f db_make
+
+while read -r ${db_coll}/nr.*.tar.gz; do
+    ls ${db_coll}/nr.*.tar.gz | parallel -j 10 -n1 -I% db_make % ${db_fastas} ${db_fastas}/nr.*.fasta ${output}/nr 
+done
+
+#db_fastas used can now be deleted
+if [[ -e ${db_fastas} ]]; then
+  rm -rf ${db_fastas} 
+fi
+
+
+
+
+
+#ls ${db_fasta}/nr.*.tar.gz | cut -d '.' -f1,2 | parallel -j 10 -n1 -I% "db_make % ${output}/nr"
+
+parallel -j 10 -n1 -I% db_make ${db_fasta}/nr.*.tar.gz ${output}/nr
+
+
+#ls ${db_fasta}/nr.*.tar.gz | cut -d '.' f1,2 | parallel -j 10 -n1 -I% "DBX % ${output}"
+#ls | cut -d '.' f1,2 | parallel -j 10 -n1 -I% "DBX % ${fasta} ${wdir} ${output}"
