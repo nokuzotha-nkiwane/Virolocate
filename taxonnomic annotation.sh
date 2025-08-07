2478aa5a3fdf03bb342e196780e76dc375787d3f
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-07 05:10:57 +0200

    test annotation

--- taxonnomic annotation.sh
+++ taxonnomic annotation.sh
@@ -1,85 +1,140 @@
 #!/bin/env bash
 
-#load modules 
-ON="module miniconda && conda activate ncbi-datasets"
+# #load modules
+ON="module miniconda && conda activate megahit"
 eval ${ON}
+module diamond
 
 #directories
-wdir="/analyses/users/nokuzothan/disc_pipe/init_tools/diamond/output"
-rv_dir="${wdir}/RVDB"
-nc_dir="${wdir}/NCBI"
-rv_ann="${rv_dir}/annotated"
-nc_ann="${nc_dir}/annotated"
+wdir="/analyses/users/nokuzothan/disc_pipe/"
+prot_db="${wdir}/ncbidb/RVDB/v30.0/U-RVDBv30.0-prot.fasta"
+nuc_db="${wdir}/ncbidb/RVDB/v30.0/U-RVDBvCurrent.fasta" #nucleotide therefore current also check what you cut 
+play="${wdir}/playground"
+output1="${play}/fastas_1"
+output2="${play}/megahit"
+output3="${play}/diamond"
+output4="${play}/asn1"
 
-#clear existing annotation directories and make new ones in RVDB and NCBI directories
-for folder in (ls ${wdir})/*; do
-    if [[ -e ${folder}/annotated ]]; then
-        rm -rf ${folder}/annotated
-    fi
+# #make playground directory unless it exists
+mkdir -p ${play}
 
-    ann_dir="${folder}/annotated"
-    mkdir -p "${ann_dir}"
-done
+#remove output1 folder if it exists
+if [[ -e ${output1} ]]; then
+  rm -rf ${output1} 
+fi
+mkdir -p ${output1}
 
-#get nucleotide accession number for RVDB samples
-awk '{print $2}' (ls ${rv_dir}/*.m8) | cut -d '|' -f5  >> "${rv_ann}/acc_id_list.txt"
+#take acc_id from RVDB nucleotide file (up to 10 acc_ids) (taking nucleotide because as much as diamond works with proteins, we would have sequences the nucleotide seq)
+cat ${nuc_db} | grep ">" | cut -d "|" -f3 | head -n 10 >> ${output1}/acc_ids.txt
 
-#function to convert NCBI protein accession to nucleotide accession
-function prot_to_nt() {
-    prot_acc=$1
-    output=$2
-    url="https://api.ncbi.nlm.nih.gov/datasets/v2alpha/protein/accession/${prot_acc}"
+#function to download fastas
+function fasta_dwnl {
+acc_id=$1
+output=$2
 
-    nt=`curl ${url} | jq -r '.protein.annotated_rna.accession // "NA"'`
-    echo -e "${prot_acc}\t${nt}" >> ${output}
+url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${acc_id}&rettype=fasta&retmode=text" 
+
+curl -N -# ${url} >> ${output}/${acc_id}.fasta
+echo "Fasta for ${acc_id} found."
+
 }
-export -f prot_to_nt
+export -f fasta_dwnl
 
-#get nucleotide accession number for NCBI samples
-awk '{print $2}' (ls ${nc_dir}/*.m8) | sort -u > "${nc_ann}/prot_id_list.txt"
+#remove output2 folder if it exists
+if [[ -e ${output2} ]]; then
+  rm -rf ${output2} 
+fi
+mkdir -p ${output2}
 
-while read -r prot_acc;do
-    prot_to_nt ${prot_acc} "${nc_ann}/acc_id_list.txt"
-    echo "Nucleotide accession found for ${prot_acc}"
-done < "${nc_ann}/prot_id_list.txt"
+#call to download fastas
+while read -r acc_id;do
+   fasta_dwnl ${acc_id} ${output1}
+done < ${output1}/acc_ids.txt
 
-
-#function to run each accession_id list through datasets to get taxonomic id
-function taxon_info() {
-    acc_id=$1
+#megahit
+function assemble {
+    fasta=$1
     output=$2
-    url="https://api.ncbi.nlm.nih.gov/datasets/v2/genome/accession/${acc_id}/"
-    #url2=""
-    
-    #get taxonomic ids acc accession id
-    meta_data=`curl -s ${url}`
-    tax_id=`echo ${meta_data} | jq -r ' .genome.taxonomy.taxonomy_id // "NA"'`
-    name=`echo ${meta_data} | jq -r ' .genome.taxonomy.organism // "NA"'`
-    rank=`echo ${meta_data} | jq -r ' .genome.taxonomy.rank // "NA"'`
-    lineage=`echo ${meta_data} | jq -r 'try ([.genome.taxonomy.lineage[].organism] | join("\t")) catch "NA"'`
-    
-    #print acccession id and taxon info
-    echo -e "${acc_id}\t${tax_id}\t${name}\t${rank}\t${lineage}" >> ${output}
+
+    base=`basename ${fasta}`
+    name=${base%.fasta}
+
+    out=${output}/${name}
+    megahit --verbose -t 10 -r ${fasta} -o ${out}
 }
-export -f taxon_info
 
-#sort and remove duplications in lists
-for ann_dir in (ls ${wdir}/*/annotated); do
-    if [[ -f ${ann_dir}/acc_id_list.txt ]]; then
-        sort -u "${ann_dir}/acc_id_list.txt" -o "${ann_dir}/unique_acc_id.txt"
-
-    #get taxon_info for each annotated directory
-    while read -r acc_id;do
-    taxon_info "${acc_id}" "${ann_dir}/taxonomy.tsv"
-    
-    #progress check
-    echo "Taxonomic information for ${acc_id} retrieved"
-    done < "${ann_dir}/unique_acc_id.txt"
-    fi
+#call to run megahit
+for fasta_file in ${output1}/*.fasta; do
+    assemble ${fasta_file} ${output2}
 done
 
 
-#cross-ref taxonomic info
+#remove output3 folder if it exists
+if [[ -e ${output3} ]]; then
+  rm -rf ${output3} 
+fi
+mkdir -p ${output3}
+
+#make diamond protein database
+diamond makedb --in ${prot_db} -d ${output3}/nr
+
+#loop through each of the files created in megahit output directory to find final.contigs.fa files and run diamond
+
+function blasting {
+  contigs=$1
+  output=$2
+ 
+  #extract sample name
+  name=`basename $(dirname ${contigs})`
+  sample_out=${output}/${name}.matches.m8
+
+  #blastx alignment
+  if [[ -f ${contigs} ]]; then
+  diamond blastx -d ${output}/nr.dmnd -q ${contigs} -o ${sample_out} --threads ${threads} -e 1E-5 -f 6 qseqid qlen sseqid stitle pident length evalue 
+
+  else 
+    echo "Contigs file for ${name} not found."
+  fi
+}
+export -f blasting
+
+#function call for alignment
+#ls ${output2}/*/final.contigs.fa | parallel -j 10 -n1 -I% "blasting % ${output3}/"
+
+for contigs in ${output2}/*/final.contigs.fa;do
+   blasting ${contigs} ${output3}
+done
+
+
+#remove output4 folder if it exists
+if [[ -e ${output4} ]]; then
+  rm -rf ${output4} 
+fi
+mkdir -p ${output4}
+
+#take acc_id from diamond output file 
+cat ${input} | grep ">" | cut -d "|" -f3 | head -n 10 >> ${output4}/acc_ids.txt
+
+#function to download asn.1 info (taxonomy)
+function taxon_dwnl() {
+acc_id=$1
+output=$2
+
+#download from protein database because based on protein database reference
+url="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=${acc_id}&rettype=asn1&retmode=text" 
+
+curl -N -# ${url} > ${output}/${acc_id}.asn1
+
+}
+export -f taxon_dwnl
+
+#link to download fastas
+while read -r acc_id;do
+   taxon_dwnl ${acc_id} ${output4}
+done < ${output4}/acc_ids.txt
+
+
+
 
 
 #  cat $file | awk '{print $2}' | cut -d '|' -f5  >>${ann_dir}/acc_id_list.txt
