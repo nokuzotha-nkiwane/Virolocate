f24e5aa590fdb83cad1dc275b98bfa0a552af90b
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-21 10:28:35 +0200

    append sample name to final.contig.fa contig names

--- megahit.sh
+++ megahit.sh
@@ -1,11 +1,20 @@
 #!/bin/env bash
 
+#load modules
 ON="module miniconda && conda activate megahit"
 eval $ON
 
+#directories
 input="/analyses/users/nokuzothan/disc_pipe/init_tools/fastp_test/output"
 output="/analyses/users/nokuzothan/disc_pipe/init_tools/megahit/output/default"
 
+#remove output directory if already exists and make new one
+if [[ -d ${output} ]]; then
+	rm -rf ${output}
+fi
+mkdir -p ${output}
+
+#run megahit
 for file in ${input}/*__R1_001_out.fastq;do 
 
 	file_name=$(basename "$file")
@@ -16,11 +25,11 @@ for file in ${input}/*__R1_001_out.fastq;do
 
 	megahit --verbose -t 10 -1 ${R1} -2 ${R2} -o ${output}/${id}
 
+	#after megahit run, prepend sample name to contigs (allows easier tracking of which contigs came from which sample in downstream analysis)
+	awk -v prefix="${id}:" '
+        /^>/ {$0=">" prefix substr($0,2)} {print}
+    ' ${output}/${id}/final.contigs.fa > ${output}/${id}/sample.contigs.fa
+
 done
 
-echo "Megahit assmebly completed successfully :-)"
-
-
-21, 27, 33
-
--meta flag for spades
+echo "Megahit assmebly completed successfully :-) and contigs named with sample name"
