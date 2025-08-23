27102f065654a1789c2665ad1b737cd0e5ce3680
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-23 19:19:03 +0200

    variables and file paths updated for clarity

--- lineage_and_filter.sh
+++ lineage_and_filter.sh
@@ -8,39 +8,37 @@ eval ${ON}
 wdir="/analyses/users/nokuzothan/disc_pipe/init_tools"
 out="${wdir}/diamond/output"
 acc_tax_id="${out}/acc_tax_id.tsv"
-lineage_out="${out}/lineages.tsv"
-mega_conts="${wdir}/megahit/output/default"
+lineage_out="${out}/all_contigs_lineages.tsv"
+fin_fasta="${wdir}/megahit/output/default/*/sample.contigs.fa"
+viral_contigs="${out}/viral_contig_lineages.tsv"
+u_match_out="${out}/viral_contig_ids_only.txt"
+output_fa="${out}/blastn.fasta"
 
 
-#clear lineage output files
+#clear lineage output file
 > ${lineage_out}
 
-#get raw lineage information
+#get lineage information for all contigs
 taxonkit lineage -d $'\t' -i 3 ${acc_tax_id} > ${lineage_out}
 
 #contig filtering according to kingdom viruses
 #empty files before extraction
-> ${out}/contig_matches.tsv
-
-u_match_out="${out}/unique_contig_ids.txt"
+> ${viral_contigs}
 > ${u_match_out}
-
-output_fa="${out}/blast_fasta.fa"
 > ${output_fa}
 
 #extract contig matches that are part of viruses
 while IFS=$'\t' read -r col1 col2 col3 col4 rest; do
-    if [[ ${col4} == *Viruses* ]]; then
-        echo -e "${col1}\t${col2}\t${col4}\t${rest}" >> ${out}/contig_matches.tsv
+    if [[ ${col4} == *Virus* ]]; then
+        echo -e "${col1}\t${col2}\t${col4}\t${rest}" >> ${viral_contigs}
     fi
 done < ${lineage_out}
 
 #get unique contig matches
-cat ${out}/contig_matches.tsv | awk '{print $1}' > ${u_match_out}
+cat ${viral_contigs} | awk '{print $1}' > ${u_match_out}
 sort -u ${u_match_out} -o ${u_match_out}
 
 #find the contig matches in the final.contigs.fa file
-fin_fasta=${mega_conts}/*/sample.contigs.fa
 
 while read -r hit; do
     awk -v contig=">${hit} " '
