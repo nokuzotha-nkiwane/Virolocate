d0465b89761c6d4e94d680f76e846682ea3dc15f
Nokuzotha Nkiwane <nokuzotha.nkiwane@gmail.com>
2025-08-31 19:04:35 +0200

    remove nr database search

--- v0/diamond_ncbi.sh
+++ v0/diamond_ncbi.sh
@@ -7,7 +7,7 @@ module diamond
 wdir="/analyses/users/nokuzothan/disc_pipe"
 cdir="${wdir}/init_tools/diamond"
 input_reads_dir="${wdir}/init_tools/megahit/output/default"
-db="${wdir}/init_tools/ncbi_fasta.faa"
+db="${wdir}/ncbidb/fasta/nr.faa"
 output="${cdir}/output/NCBI"
 tmp_db="${output}/nt.tmp"
 threads=$((`/bin/nproc` -2))
@@ -17,48 +17,6 @@ if [[ -e $output ]]; then
   rm -rf ${output} 
 fi
 mkdir -p -m a=rwx ${output}
-
-
-
-
-
-while read -r virus; do
-    grep -i "^>.*${virus}" ${db} | \
-    awk '
-        /^>/ {title=$0; next}
-        {print title "\n" $0}
-    ' >> ncbi_fasta
-done < virus.txt
-
-wdir="/analyses/users/nokuzothan/disc_pipe"
-db="${wdir}/ncbidb/fasta/nr.faa"
-
-while read -r virus; do
-    awk -v name="${virus}" '
-        BEGIN {IGNORECASE=1}
-        /^>/ {ON = index($0, name) > 0}
-        ON {print}
-    ' ${db} >> ncbi_fasta
-done < virus.txt
-
-#downloading protein fasta sequences from genbank and making them one file 
-
-wdir="/analyses/users/nokuzothan/disc_pipe"
-db="${wdir}/init_tools/ncbi_fasta.faa"
-# Number of sequences per batch
-batch=50000
-
-# Step 1: Get total number of viral protein sequences
-total=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=txid10239[Organism]&rettype=count&retmode=text")
-echo "Total viral protein sequences: $total"
-
-# Step 2: Fetch in batches
-for start in $(seq 0 $batch $total); do
-    echo "Fetching sequences $start to $((start+batch-1))..."
-    curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&term=txid10239[Organism]&retstart=${start}&retmax=${batch}&rettype=fasta&retmode=text" >> ${db}
-done
-
-echo "Download complete. Sequences saved in ${db}"
 
 
 #make diamond protein database
