30e4e2429eb6e544a029497a846db905818242dc
Nokuzotha Nkiwane <nokuzotha-nkiwane@github.com>
2025-10-06 12:13:13 +0200

    permissions changed

--- run_local.sh
+++ run_local.sh
@@ -2,15 +2,15 @@
 
 module nextflow
 
-NXF_HOME=/analyses/.nextflow
+NXF_HOME=/analyses/download/.nextflow
 
 # Set the name with random suffix in a Bash-compatible way
-NAME="test-$1-rid$RANDOM"
+NAME="test-rid$RANDOM"
 
-nextflow run 'https://github.com/nokuzotha-nkiwane/Virolocate.git' \
+nextflow run main.nf \
                  -name "$NAME" \
                  -profile singularity \
                  -c /analyses/users/nokuzothan/Virolocate/custom.config \
-                 -r develop -latest -resume  \
+                 -resume  \
                  --outdir "results/$NAME" \
                  -work-dir "work/$NAME"
