58ba6ec179ab5275db6be5779685bad3f3e35107
abhi18av <abhi18av@outlook.com>
2025-10-15 14:41:51 +0200

    new run location updates

--- run_local.sh
+++ run_local.sh
@@ -2,7 +2,7 @@
 
 module nextflow
 
-NXF_HOME=/analyses/download/.nextflow
+NXF_HOME=/analyses/projects/.nextflow
 
 # Set the name with random suffix in a Bash-compatible way
 NAME="test-rid$RANDOM"
@@ -10,10 +10,10 @@ NAME="test-rid$RANDOM"
 nextflow run main.nf \
                  -name "$NAME" \
                  -profile test,singularity \
-                 -c /analyses/download/Virolocate/_custom.config \
+                 -c /analyses/projects/Virolocate/_custom.config \
                  -resume  \
                  --outdir "results/$NAME" \
                  -work-dir "work/$NAME" \
-		 --input /analyses/download/Virolocate/s_sheet.csv 
+		 --input /analyses/projects/Virolocate/s_sheet.csv 
 
 		 #-entry VIROLOCATE_NF \
